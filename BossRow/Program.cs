d934e67fea188baaec622ca810e9216f786575d1
xLipeQ <m.lipiak03@gmail.com>
2024-11-22 21:33:55 +0100

    Splitted functions

--- BossRow/Program.cs
+++ BossRow/Program.cs
@@ -1,10 +1,19 @@
-﻿using System.Threading.Channels;
+﻿using System;
+using System.Threading.Channels;
 
 namespace BossRow
 {
     public static class Program
     {
         static void Main(string[] args)
+        {
+            Task();
+            Train();
+            Tree();
+            NotOrderedEmployees();
+        }
+
+        static void Task()
         {
             List<Employee> employees = new List<Employee>()
             {
@@ -17,20 +26,14 @@ namespace BossRow
             var func = new Funcitonalitites();
             func.FillEmployeesStructure(employees);
 
-            foreach(var e in employees)
+            foreach (var e in employees)
                 Console.WriteLine(e);
 
-            Console.WriteLine("Tabela employeeStructure dla danych z zadania: ");
-            Console.WriteLine($"{"EmployeeId",-12} {"SuperiorId",-12} {"Row",-5}");
-            Console.WriteLine(new string('-', 34));
-            foreach (var es in func.employeeStructures)
-            {
-                Console.WriteLine($"| {es.EmployeeId,-10} | {es.SuperiorId,-10} | {es.Row,-3} |");
-            }
+            Display(func);
+        }
 
-            Console.WriteLine();
-            Console.WriteLine();
-
+        static void Train()
+        {
             List<Employee> trainEmployees = new List<Employee>()
             {
                 new Employee(1, "Jan Kowalski"),
@@ -44,18 +47,14 @@ namespace BossRow
             foreach (var e in trainEmployees)
                 Console.WriteLine(e);
 
+            var func = new Funcitonalitites();
             func.FillEmployeesStructure(trainEmployees);
-            Console.WriteLine("Tabela employeeStructure dla ciągu 7 pracowników: ");
-            Console.WriteLine($"{"EmployeeId",-12} {"SuperiorId",-12} {"Row",-5}");
-            Console.WriteLine(new string('-', 34));
-            foreach (var es in func.employeeStructures)
-            {
-                Console.WriteLine($"| {es.EmployeeId,-10} | {es.SuperiorId,-10} | {es.Row,-3} |");
-            }
-            Console.WriteLine();
-            Console.WriteLine();
 
-            
+            Display(func);
+        }
+
+        static void Tree()
+        {
             List<Employee> treeEmployees = new List<Employee>()
             {
                 new Employee(1, "Jan Kowalski"),
@@ -69,14 +68,44 @@ namespace BossRow
             foreach (var e in treeEmployees)
                 Console.WriteLine(e);
 
+            var func = new Funcitonalitites();
             func.FillEmployeesStructure(treeEmployees);
-            Console.WriteLine("Tabela employeeStructure dla wyważonego drzewa 7 pracowników: ");
+
+            Display(func);
+        }
+
+        static void NotOrderedEmployees()
+        {
+            List<Employee> NotOrderedEmployees = new List<Employee>()
+            {
+                new Employee(5, "Jan Kowalski", 3),
+                new Employee(6, "Kamil Nowak", 3),
+                new Employee(2, "Anna Mariacka", 5),
+                new Employee(1, "Andrzej Abacki", 6),
+                new Employee(3, "ssss", 4),
+                new Employee(7, "ffff", 4),
+                new Employee(4, "aaaa")
+            };
+            foreach (var e in NotOrderedEmployees)
+                Console.WriteLine(e);
+
+            var func = new Funcitonalitites();
+            func.FillEmployeesStructure(NotOrderedEmployees);
+            Display(func);
+        }
+
+        static void Display(Funcitonalitites func)
+        {
+            Console.WriteLine("Tabela employeeStructure dla powyższych pracowników: ");
             Console.WriteLine($"{"EmployeeId",-12} {"SuperiorId",-12} {"Row",-5}");
             Console.WriteLine(new string('-', 34));
             foreach (var es in func.employeeStructures)
             {
                 Console.WriteLine($"| {es.EmployeeId,-10} | {es.SuperiorId,-10} | {es.Row,-3} |");
             }
+
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
